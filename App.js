eb4c2ab4745e783db9e1ac8c41c22339cf2bc4e7
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-10-10 13:09:21 -0400

    added firebase to application and passed the firestore db as a prop to chat

--- App.js
+++ App.js
@@ -2,15 +2,36 @@ import StartApp from './components/Start';
 import ChattyScreen from './components/Chat';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { initializeApp } from 'firebase/app';
+import { initializeFirestore } from 'firebase/firestore';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  const firebaseConfig = {
+    apiKey: 'AIzaSyAWnfpvCHIwaKD0wYaUCbH9pqbAu4NhyAY',
+    authDomain: 'chatapp-f783f.firebaseapp.com',
+    projectId: 'chatapp-f783f',
+    storageBucket: 'chatapp-f783f.appspot.com',
+    messagingSenderId: '751916694021',
+    appId: '1:751916694021:web:42126664c67d40b06cba6c'
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = initializeFirestore(app, {
+    experimentalForceLongPolling: true
+  });
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Start'>
         <Stack.Screen name='Home' component={StartApp} />
-        <Stack.Screen name='ChatScreen' component={ChattyScreen} />
+        <Stack.Screen name='Chat'>
+          {(props) => <ChattyScreen db={db} {...props} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
