a9fe242649dd77880ee04c848fd49168a3223105
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-10-10 13:11:10 -0400

    added messages functionality to save to firebase

--- components/Chat.js
+++ components/Chat.js
@@ -1,36 +1,41 @@
-import { StyleSheet, View, Text, KeyboardAvoidingView } from 'react-native';
+import { StyleSheet, View, KeyboardAvoidingView } from 'react-native';
 import { useEffect, useState } from 'react';
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
+import {
+  collection,
+  addDoc,
+  onSnapshot,
+  query,
+  orderBy
+} from 'firebase/firestore';
 
-const ChattyScreen = ({ route, navigation }) => {
-  const { name, pickColor } = route.params;
+const ChattyScreen = ({ route, navigation, db }) => {
+  const { name, pickColor, userID } = route.params;
   const [messages, setMessages] = useState([]);
 
   const onSend = (newMessages) => {
-    setMessages((previousMessages) =>
-      GiftedChat.append(previousMessages, newMessages)
-    );
+    addDoc(collection(db, 'messages'), newMessages[0]);
   };
 
   useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: 'Hello developer',
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any'
-        }
-      },
-      {
-        _id: 2,
-        text: `${name} has just entered the chat`,
-        createdAt: new Date(),
-        system: true
-      }
-    ]);
+    const q = query(collection(db, 'messages'), orderBy('createdAt', 'desc'));
+
+    const unsubMessages = onSnapshot(q, (docs) => {
+      let newMessages = [];
+      docs.forEach((doc) => {
+        newMessages.push({
+          id: doc.id,
+          ...doc.data(),
+          createdAt: new Date(doc.data().createdAt.toMillis())
+        });
+      });
+      setMessages(newMessages);
+    });
+
+    // Clean up code
+    return () => {
+      if (unsubMessages) unsubMessages();
+    };
   }, []);
 
   useEffect(() => {
@@ -60,7 +65,8 @@ const ChattyScreen = ({ route, navigation }) => {
         messages={messages}
         onSend={(messages) => onSend(messages)}
         user={{
-          _id: 1
+          _id: userID,
+          name
         }}
       />
       {Platform.OS === 'android' ? (
