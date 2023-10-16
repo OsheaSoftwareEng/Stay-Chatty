658c3dfb546f24a9583c86840f7691a0554ae9db
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-10-16 13:16:02 -0400

    added cached messages to save messages when offline

--- components/Chat.js
+++ components/Chat.js
@@ -1,6 +1,7 @@
 import { StyleSheet, View, KeyboardAvoidingView } from 'react-native';
 import { useEffect, useState } from 'react';
-import { GiftedChat, Bubble } from 'react-native-gifted-chat';
+import { GiftedChat, Bubble, InputToolbar } from 'react-native-gifted-chat';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {
   collection,
   addDoc,
@@ -9,7 +10,7 @@ import {
   orderBy
 } from 'firebase/firestore';
 
-const ChattyScreen = ({ route, navigation, db }) => {
+const ChattyScreen = ({ route, navigation, db, isConnected }) => {
   const { name, pickColor, userID } = route.params;
   const [messages, setMessages] = useState([]);
 
@@ -18,29 +19,60 @@ const ChattyScreen = ({ route, navigation, db }) => {
   };
 
   useEffect(() => {
-    const q = query(collection(db, 'messages'), orderBy('createdAt', 'desc'));
+    let unsubMessages;
+    if (isConnected === true) {
+      if (unsubMessages) unsubMessages();
+      unsubMessages = null;
 
-    const unsubMessages = onSnapshot(q, (docs) => {
-      let newMessages = [];
-      docs.forEach((doc) => {
-        newMessages.push({
-          id: doc.id,
-          ...doc.data(),
-          createdAt: new Date(doc.data().createdAt.toMillis())
+      const q = query(collection(db, 'messages'), orderBy('createdAt', 'desc'));
+
+      unsubMessages = onSnapshot(q, (docs) => {
+        let newMessages = [];
+        docs.forEach((doc) => {
+          newMessages.push({
+            id: doc.id,
+            ...doc.data(),
+            createdAt: new Date(doc.data().createdAt.toMillis())
+          });
         });
+        cacheMessages(newMessages);
+        setMessages(newMessages);
       });
-      setMessages(newMessages);
-    });
+    } else loadCachedLists();
 
     // Clean up code
     return () => {
       if (unsubMessages) unsubMessages();
     };
-  }, []);
+  }, [isConnected]);
+
+  //caches and saves messages with async storage
+  const cacheMessages = async (messagesToCache) => {
+    try {
+      await AsyncStorage.setItem(
+        'new_messages',
+        JSON.stringify(messagesToCache)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   useEffect(() => {
     navigation.setOptions({ title: name });
   }, []);
+
+  //goes through cached messages and parses them back to the original state to be displayed if user loses connection
+  const loadCachedLists = async () => {
+    const cachedMessages = (await AsyncStorage.getItem('new_messages')) || [];
+    setMessages(JSON.parse(cachedMessages));
+  };
+
+  //disables toolbar if user loses connection
+  const renderInputToolbar = (props) => {
+    if (isConnected) return <InputToolbar {...props} />;
+    else return null;
+  };
 
   //sets bubble color for chat text boxes
   const renderBubble = (props) => {
@@ -61,6 +93,7 @@ const ChattyScreen = ({ route, navigation, db }) => {
   return (
     <View style={[styles.container, { backgroundColor: pickColor }]}>
       <GiftedChat
+        renderInputToolbar={renderInputToolbar}
         renderBubble={renderBubble}
         messages={messages}
         onSend={(messages) => onSend(messages)}
